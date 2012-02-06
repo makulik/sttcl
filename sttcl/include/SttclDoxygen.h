2b5c3a00fd6a1fbde296aa5290cb0013f1e131a9
unknown <Admin@.(none)>
2012-02-06 20:07:35 +0100

    Added Doxygen documentation
    Signed-off-by: unknown <g-makulik@t-online.de>

--- sttcl/include/SttclDoxygen.h
+++ sttcl/include/SttclDoxygen.h
@@ -17,6 +17,7 @@
  * \ref usage_sec_2 "2.2 Declare State implementations"\n
  * \ref usage_sec_3 "2.3 Implement the state machine interfaces"\n
  * \ref usage_sec_4 "2.4 Implement state actions and transitions"\n
+ * \ref uml2gof_sec "3 Mapping of the UML state diagram notation elements to the GoF State pattern"
  *
  * \section intro_sec 1 Introduction
  *
@@ -38,14 +39,89 @@
 /**
  * \page usage_page Usage of the STTCL state machine template class framework
  * \section usage_sec 2 Usage of the STTCL state machine template class framework
+ * \ref usage_sec_1 "2.1 Declare a StateMachine implementation"\n
+ * \ref usage_sec_2 "2.2 Declare State implementations"\n
+ * \ref usage_sec_3 "2.3 Implement the state machine interfaces"\n
+ * \ref usage_sec_4 "2.4 Implement state actions and transitions"\n
  *
  * \subsection usage_sec_1 2.1 Declare a StateMachine implementation
  *
+ * Analyze your UML 2.2 state diagram and declare all necessary variables and event (trigger
+ * methods) needed for the state machine. Declare an appropriate instance of \c sttcl::StateMachine<>
+ * as base class for your state machine class.
+ *
+ * \code
+class MyStateMachine;
+
+class IStateEventHandlers
+{
+public:
+	virtual handleEvent1(MyStateMachine* context) = 0;
+	virtual handleEvent2(MyStateMachine* context) = 0;
+};
+
+class MyStateMachine : public sttcl::StateMachine<MyStateMachine,IStateEventHandlers>
+{
+public:
+	void event1();
+	void event2();
+
+	int getX() const;
+	const std::string& getY();
+
+	// ...
+};
+\endcode
+ *
+ * According to the GoF state pattern implementation variants you can decide where to hold the
+ * necessary state instances and make their sibling states accessible for them, so they can
+ * realize event triggered transitions.
+ * As a rule of thumb the state implementation classes can be instantiated as singletons (see
+ * <A HREF="http://en.wikipedia.org/wiki/Singleton_Pattern">GoF Singleton pattern</A>) if they
+ * have no member variables other than application scope configurations. Otherwise the available
+ * states should be embedded members of the state machine implementation class and are accessible
+ * from the state machine context for state implementation classes.
+ *
+ * \note State implementation classes inheriting from the sttcl::ActiveState<>
+ * or sttcl::CompositeState<> template classes implicitly have embedded members in the base
+ * classes.
+ *
  * \subsection usage_sec_2 2.2 Declare State implementations
+ * Implement a state class for each state in your UML 2.2 state machine diagram. Declare the
+ * appropriate instance of the sttcl::State<> template class as base class for all state classes.
+ * \code
+class MyStateMachine;
+
+class MyState1 : public sttcl::State<MyState1,MyStateMachine,IStateEventHandlers>
+{
+	// ...
+};
+\endcode
  *
  * \subsection usage_sec_3 2.3 Implement state machine interface
  *
+ * The event1() and event2() method implementations of MyStateMachine can just delegate to the
+ * matching call to the IStateEventHandlers interface of the current state
+ * \code
+void MyStateMachine::event1()
+{
+	IStateEventHandlers* currentState = getState();
+	if(currentState)
+	{
+		currentState->handleEvent1(this);
+	}
+}
+\endcode
+ *
  * \subsection usage_sec_4 2.4 Implement state actions and transitions
+ *
+ * Transition actions are implemented within the event handler methods of the state classes.
+ */
+
+/**
+ * \page uml2gof_page Mapping of the state diagram notation elements
+ * \section uml2gof_sec 3 Mapping of the UML state diagram notation elements to the GoF State pattern
+ * \htmlinclude UMLStateGoFStateMapping.html
  */
 
 /**
